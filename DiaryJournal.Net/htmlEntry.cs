afdfc323684091a715306129d6cc150a3fad69d6
tusharjain1082 <43837276+tusharjain1082@users.noreply.github.com>
2023-03-27 23:29:09 +0530

    upgraded to version 1.8.22.0
    upgraded to version 1.8.22.0

---
+++ DiaryJournal.Net/htmlEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RtfPipe;
+
+namespace DiaryJournal.Net
+{
+    public static class htmlEntry
+    {
+        public static String toHtml(String rtf)
+        {
+            // get rtf and update
+            if (rtf.Length <= 0)
+                return "";
+
+            String html = Rtf.ToHtml(rtf);
+            return html;
+        }
+    }
+}
